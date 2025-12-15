af03a452aa67d3b73432d68b5dbb3cfc9aa2efe9
Muhamad Naufal Afrizal <naufalafrizal06@gmail.com>
2025-12-15 19:32:56 +0700

    Add files via upload

---
+++ praktikum5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main() {
+	int data[11] = { 12,17,10,5,15,25,11,7,25,16,19 };
+
+	vector <int> myvector(data, data + 11);
+
+	auto cetakVector = [](const vector<int> vec, const string& judul)
+		{
+			cout << judul << endl;
+			for (int val : vec)
+			{
+				cout << val << " | ";
+			}
+			cout << "\n====================================================\n";
+		};
+	cetakVector(myvector, "isi vector awal :");
+	
+	sort(myvector.begin(), myvector.end());
+	cetakVector(myvector, "isi vector setelah asc :");
+
+	sort(myvector.rbegin(), myvector.rend());
+	cetakVector(myvector, "isi vector setelah desc :");
+	
+}
